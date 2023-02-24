05fc39e5848f444ec1f9aaf9a8e66a2f5092c483
Sinthon <s.sinthon@cas.com.kh>
2023-02-24 09:20:05 +0700

    Add project files.

---
+++ wpf_caliburn_micro/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using wpf_caliburn_micro.ViewModels;
+
+namespace wpf_caliburn_micro
+{
+    internal class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<MainViewModel>();
+        }
+    }
+}
