7db314e47aa3cd7a8377e5c66c9aea94ad0fddcc
Sinthon <s.sinthon@cas.com.kh>
2023-02-24 12:04:26 +0700

    add testingRelay command

---
+++ wpf_caliburn_micro/ViewModels/Commands/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace wpf_caliburn_micro.ViewModels.Commands
+{
+    internal class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value;}
+        }
+
+        private Action<object?>? execute { get; set; }
+        private Func<object?, bool>? canExecute { get; set; }
+
+
+        public RelayCommand(Action<object?>? execute, Func<object?, bool>? canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute?.Invoke(parameter);
+        }
+    }
+}
--- wpf_caliburn_micro/ViewModels/MainViewModel.cs
+++ wpf_caliburn_micro/ViewModels/MainViewModel.cs
@@ -4,10 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using wpf_caliburn_micro.ViewModels.Commands;
 
 namespace wpf_caliburn_micro.ViewModels
 {
     internal class MainViewModel : Screen
     {
+        public ICommand TestingRelayCommand { get; }
+
+        public MainViewModel()
+        {
+            TestingRelayCommand = new RelayCommand((object? param) =>
+            {
+                MessageBox.Show(param?.ToString() ?? "");
+            });
+        }
     }
 }
